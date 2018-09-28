2fcd302e4f95573efdeabdb13503623a85f8b359
Abezer <abezes.haile@gmail.com>
2018-09-28 12:52:54 -0400

    Updated File

--- src/GroceryInvoice/InvoiceApp.java
+++ src/GroceryInvoice/InvoiceApp.java
@@ -11,8 +11,9 @@ public class InvoiceApp {
 
 
         Scanner input = new Scanner(System.in);
-        String data ;
+        String data = "";
         int itemsPuchased = 0;
+        double totalPurchase = 0;
         do {
             System.out.println("Enter Item Name");
             GroceryItems items = new GroceryItems();
@@ -26,27 +27,30 @@ public class InvoiceApp {
 
             System.out.println("Enter Item Quantity");
             int numberOfItem = input.nextInt();
-            items.setQuantity(numberOfItem);
+            input.nextLine();
 
+            items.setQuantity(numberOfItem);
             double cost = numberOfItem * priceItem;
             items.setCost(cost);
+            totalPurchase = cost + totalPurchase;
             myItems.add(items);
             itemsPuchased++;
 
             System.out.println("Do you have another Item? Yes or No");
+
             data = input.nextLine();
+
 
         } while (data.equalsIgnoreCase("Yes"));
 
-
+        System.out.println("Item Name\t\t\tPrice\t\tQuantity\t\tCost");
         for (GroceryItems obj : myItems) {
 
-            System.out.println("Name: " + obj.getItemName() + "\n" + "Price: " + obj.getPrice()
-                    + "\n" + "Quantity: " + obj.getQuantity()
-                    + "\n" + "Cost:" + obj.getCost());
+            System.out.println(obj.getItemName()+"\t\t\t\t"+obj.getPrice()
+                   + "\t\t\t"+obj.getQuantity()+"\t\t\t"+obj.getCost());
         }
 
-        System.out.println("Total Purchase Amount = ");
+        System.out.println("Total Purchase Amount = " + totalPurchase);
 
         System.out.println("Total Items Purchased: " + itemsPuchased);
 
